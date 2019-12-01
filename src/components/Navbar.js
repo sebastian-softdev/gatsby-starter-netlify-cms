e11baf74fc491231c150fc0147f8d613c78a8f20
Sebastian <sebastian@softdev.team>
2019-12-01 13:49:33 -0500

    added tags to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
+              <Link className="navbar-item" to="/tags">
+                Tags
+              </Link>
               <Link className="navbar-item" to="/blog">
                 Patterns
               </Link>
