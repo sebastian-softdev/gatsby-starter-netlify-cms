600fbd08ec9eacaec505e156d29d7d654edc1fd0
Sebastian <sebastian@softdev.team>
2019-12-01 13:28:44 -0500

    quick ui tweak for demo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,11 +63,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Patterns
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
